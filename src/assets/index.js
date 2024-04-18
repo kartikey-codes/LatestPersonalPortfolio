f16cc76facb1310e91ad76c5e38fcda31ea318ec
kartkey-codes <kartikey.bhatnagar2021@vitstudent.ac.in>
2024-04-18 18:01:02 +0530

    removed extra assests

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,8 +1,5 @@
 import logo from "./logo.svg";
 import backend from "./backend.png";
-import creator from "./creator.png";
-import mobile from "./mobile.png";
-import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
@@ -44,9 +41,7 @@ import Selenium from "./tech/Selenium.png";
 import Vercel from "./tech/Vercel.png";
 import YAML from "./tech/YAML.png";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
+
 import tesla from "./company/tesla.png";
 import innovya from "./company/innovya.png";
 
@@ -68,9 +63,6 @@ export {
   logo,
   Mozillavit,
   backend,
-  creator,
-  mobile,
-  web,
   github,
   menu,
   close,
